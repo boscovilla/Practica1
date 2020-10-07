ba73155964c82728747f9f39f1a9e5e1b9502c4e
Axel0077 <axelleonardoperezm@gmail.com>
2020-10-06 19:48:40 -0600

    Corrigiendo Identacion de codigo y borrando paquetes que no se utilizan

--- Practica1/Controllers/MusicControllers/GeneroesController.cs
+++ Practica1/Controllers/MusicControllers/GeneroesController.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Practica1.Data;
 using Practica1.Models;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Practica1.Controllers
 {
